ab869de517344e9960a189db7582fa188a4d0cf5
Kamil Lewandowski <kamlew555@gmail.com>
2023-08-19 15:01:17 +0200

    create handleSubmit function and fix bug with event

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,24 @@
 const ratingBtnEls = document.querySelectorAll(".rating__btn");
 const submitBtnEl = document.querySelector(".rating__submit__btn");
 const popupEl = document.querySelector(".rating__popup");
-const selectedNumberEL = document.querySelector(".rating__number");
+const selectedNumberEl = document.querySelector(".rating__number");
 const ratingContainerEl = document.querySelector(".rating__container");
-
 
 
 const handleRate = (e) => {
 	const btn = e.target;
-    const rating = parseInt(btn.getAttribute("data-rating"))
-    selectedNumberEL.textContent = `${rating}`;
-    console.log(selectedNumberEL);
+	const rating = parseInt(btn.getAttribute("data-rating"));
+	selectedNumberEl.textContent = rating;
 
 };
 
+const handleSubmitClick = () => {
+	if (selectedNumberEl.textContent !== 0) {
+		popupEl.style.display = "block";
+		ratingContainerEl.style.display = "none";
+	}
+};
 
+ratingBtnEls.forEach(btn => btn.addEventListener("click", handleRate));
 
-
-ratingBtnEls.forEach((btn) => addEventListener("click", handleRate));
+submitBtnEl.addEventListener("click", handleSubmitClick);
