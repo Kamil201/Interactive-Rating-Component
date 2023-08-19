06432cdb725ff2bafb981aa9a59042fabc928701
Kamil Lewandowski <kamlew555@gmail.com>
2023-08-19 12:39:10 +0200

    clear

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,19 @@
-const ratingBtnEls = document.querySelectorAll('.rating__btn')
+const ratingBtnEls = document.querySelectorAll(".rating__btn");
 const submitBtnEl = document.querySelector(".rating__submit__btn");
-const popupEl = document.querySelector('.rating__popup')
+const popupEl = document.querySelector(".rating__popup");
 const selectedNumberEL = document.querySelector(".rating__number");
-const ratingContainerEl = document.querySelector('.rating__container')
+const ratingContainerEl = document.querySelector(".rating__container");
+
+
+
+const handleRate = (e) => {
+	const btn = e.target;
+    const rating = parseInt(btn.getAttribute("data-rating"))
+    selectedNumberEL.textContent = `${rating}`
+
+};
 
 
 
 
+ratingBtnEls.forEach((btn) => addEventListener("click", handleRate));
