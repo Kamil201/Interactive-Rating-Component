9b7cb913bcfda78471911575cf5485f279c3f0e1
Kamil Lewandowski <kamlew555@gmail.com>
2023-08-19 11:47:55 +0200

    save references to elements into veriables

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 const ratingBtnEls = document.querySelectorAll('.rating__btn')
 const submitBtnEl = document.querySelector(".rating__submit__btn");
 const popupEl = document.querySelector('.rating__popup')
+const selectedNumberEL = document.querySelector(".rating__number");
+const ratingContainerEl = document.querySelector('.rating__container')
 
 
 
